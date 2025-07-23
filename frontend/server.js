46bdba83ead20d0e8e2e6452b37d5e6c364d6b2c
kassoum sanogo <kassoum.sanogo@outlook.com>
2025-07-23 15:06:38 -0400

    update

--- frontend/server.js
+++ frontend/server.js
@@ -7,7 +7,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const API_URL = process.env.API_URL || 'http://127.0.0.1:8000';
+const API_URL = process.env.API_URL || 'http://127.0.0.1:8010';
 
 // Configuration de sécurité
 app.use(helmet({
